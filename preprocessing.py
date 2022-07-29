38822c5cfb5e41caf6175e345461b2076366f736
DhouhaAR <79200476+DhouhaAR@users.noreply.github.com>
2022-07-29 19:41:04 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -14,10 +14,10 @@ raw_data_path = os.path.join(RAW_DATA_DIR, RAW_DATA_FILE)
 data = pd.read_csv(raw_data_path, sep=",")
 
 # Drop useless variables
-X = data.drop(['target'], axis='columns')
-y = data['target'].to_frame()
+X = data.drop(['Outcome'], axis='columns')
+y = data['Outcome'].to_frame()
 
-train, test = train_test_split(data, test_size=0.3, stratify=data['target'])
+train, test = train_test_split(data, test_size=0.3, stratify=data['Outcome'])
 
 # Set path to the outputs
 PROCESSED_DATA_DIR = os.environ["PROCESSED_DATA_DIR"]
